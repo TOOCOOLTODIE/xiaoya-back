ee4933bdf854fa4e7a92c80a46707beb2b79707d
罗小叶 <1924350485@qq.com>
2022-04-02 00:07:10 +0800

    定时任务后台代码提交
    (cherry picked from commit 4eaee670a2b08dba254f7300c9be19b8f347cfb4)

--- src/main/java/com/szw/mapper/BatchRateMapper.java
+++ src/main/java/com/szw/mapper/BatchRateMapper.java
@@ -2,9 +2,11 @@ package com.szw.mapper;
 
 import com.szw.entity.Model;
 import com.szw.entity.Sort;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface BatchRateMapper {
     List<Model> getModels();
     List<Sort> getSorts();
